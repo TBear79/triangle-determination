b171638822c0c9076e0cc777a83f71db8cf6b20e
Thorbjørn Jelgren <thorbjorn@therightfoot.dk>
2021-10-21 13:33:11 +0200

    Fix tests after logic became static class

--- Tests/TriangleBl.Test.cs
+++ Tests/TriangleBl.Test.cs
@@ -1,4 +1,3 @@
-using System;
 using Xunit;
 using Shouldly;
 
@@ -7,20 +6,13 @@ namespace TriangleDetermination.Test
 
     public class TriangleBlTests
     {
-        TriangleBl sut;
-
-        public TriangleBlTests()
-        {
-            sut = new TriangleBl();
-        }
-
         [Theory]
         [InlineData(10, 10, 10, TriangleEnum.Iquilateral)]
         [InlineData(10, 10, 20, TriangleEnum.Isosceles)]
         [InlineData(10, 20, 30, TriangleEnum.Scalene)]
         public void Input_ShouldHandle(int side1, int side2, int side3, TriangleEnum expectedResult)
         {
-            var result = sut.DetermineType(side1, side2, side3);
+            var result = TriangleBl.DetermineType(side1, side2, side3);
 
             result.ShouldBe(expectedResult);
         }
